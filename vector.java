1089e9a829122c4dda83d021504ceca8b356577b
restrictor <restrictor@users.noreply.github.com>
2017-10-03 23:43:09 +0200

    Update vector.java
    getting better

--- vector.java
+++ vector.java
@@ -1,7 +1,7 @@
+import java.util.Random;
 
 public class vector implements Comparable<vector>{
 
-	
 	/**
 	 * @param args
 	 * 	vector	a 10 dimensional solution for the function at hand
@@ -31,20 +31,27 @@ public class vector implements Comparable<vector>{
 		this.solution = sol;
 	}
 	
-	vector(scalar[] scalarArray, double fitness) {
+	vector(scalar[] s) {
 		sol = new double[DIMENSIONS];
 		for (int i = 0; i < DIMENSIONS; i++) {
-			sol[i] = scalarArray[i].x;
+			sol[i] = s[i].x;
 		}
 		this.solution = sol;
-		this.scalarArray = scalarArray;
-		this.fitness = fitness;
+		this.scalarArray = s;
+		this.fitness = 0.0;
 	}
 	
-	void mutate() {
+	double[] mutate() {
+		double[] sol2 = new double[DIMENSIONS];
 		for (int i = 0; i < DIMENSIONS; i++) {
-			this.scalarArray[i].addGaussian();
-			this.solution[i] = this.scalarArray[i].x;
+			Random r = new Random();
+			sol2[i] = (r.nextGaussian()*2*this.scalarArray[i].sigma) + this.scalarArray[i].x;
+			sol2[i] = Math.min(Math.max(-5, sol2[i]),5);
 		}
+		return sol2;
+	}
+	
+	void adjustFitness(double fit) {
+		this.fitness = fit;
 	}
 }
