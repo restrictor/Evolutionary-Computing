c42436d1cd532ea932f96a0e4d7483d1d8e3fc7c
restrictor <restrictor@users.noreply.github.com>
2017-10-06 18:58:37 +0200

    Update EA.sh

--- EA.sh
+++ EA.sh
@@ -1,10 +1,15 @@
-@#!/bin/bash
 for number in 3 2 1
 do
 echo $number
 done
 echo 'start'
-java -jar testrun.jar -submission=player19 -evaluation=SchaffersEvaluation -seed=1 > Schaffers.txt
-java -jar testrun.jar -submission=player19 -evaluation=BentCigarFunction -seed=1 > BentCigar.txt
-java -jar testrun.jar -submission=player19 -evaluation=KatsuuraEvaluation -seed=1 > Katsuura.txt
+echo 'compiling'
+javac -cp contest.jar player19.java vector.java scalar.java lib.java
+jar cmf MainClass.txt submission.jar player19.class vector.class scalar.class lib.class
+echo 'running Schaffers'
+java -jar testrun.jar -submission=player19 -evaluation=SchaffersEvaluation -seed=1 #> Schaffers.txt
+echo 'running BentCigar'
+java -jar testrun.jar -submission=player19 -evaluation=BentCigarFunction -seed=1 #> BentCigar.txt
+echo 'running Katsuura'
+java -jar testrun.jar -submission=player19 -evaluation=KatsuuraEvaluation -seed=1 #> Katsuura.txt
 exit 0
