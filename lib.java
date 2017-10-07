02a2deb90501202f2a883607544f3eb6aba31b6e
restrictor <restrictor@users.noreply.github.com>
2017-10-07 13:23:41 +0200

    Update lib.java
    adding copy vector and uniformcrossover

--- lib.java
+++ lib.java
@@ -1,4 +1,6 @@
 
+import java.util.Random;
+
 public class lib {
 	
 	/**
@@ -22,5 +24,36 @@ public class lib {
 		vector a = new vector(s);
 		return a;
 	}
+
+	static vector[] uniformCrossover(vector parent1, vector parent2) {
+		Random r = new Random();		
+		vector child1 = copyVector(parent1);
+		vector child2 = copyVector(parent2);
+		for (int i = 0; i <10; i++){
+			if (Math.random()<0.5) {
+				child1.scalarArray[i].x = parent2.scalarArray[i].x;
+				child1.scalarArray[i].sigma = parent2.scalarArray[i].sigma;
+				child2.scalarArray[i].x = parent1.scalarArray[i].x;
+				child2.scalarArray[i].sigma = parent1.scalarArray[i].sigma;
+			}
+		}
+		vector[] twins = new vector[2];
+		twins[0] = child1;
+		twins[1] = child2;
+ 		return twins;
+	}
+
+	static vector copyVector(vector original) {
+		scalar[] s = new scalar[10];
+		for (int i = 0; i <10; i++){
+			s[i] = new scalar();
+			s[i].x = original.scalarArray[i].x;
+			s[i].sigma = original.scalarArray[i].sigma;  		
+		}
+		vector copy = new vector(s);
+		copy.fitness = original.fitness;
+		return copy; 
+	}
+
 }
 
